820a53576ea7715032173c34286be2f70dc14fbf
jirry.ji <jirenyoucn@gmail.com>
2021-07-13 21:33:46 +0800

    目录util

--- cli/util/DirUtil.go
+++ cli/util/DirUtil.go
@@ -1,8 +1,11 @@
 package util
 
 import (
+	"github.com/xinlianit/go-util"
 	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 	"sync"
 )
 
@@ -80,6 +83,118 @@ func (u Dir) CreateDir(dirPath string, recursive bool) error {
 			return os.MkdirAll(dirPath, os.ModePerm)
 		} else {
 			return os.Mkdir(dirPath, os.ModePerm)
+		}
+	}
+	return nil
+}
+
+// LsDir 列出目录文件
+// @param dirPath 		目录路径
+// @param readChildDir 	读取子目录
+// @return fileList 	文件列表
+// @return err
+func (u Dir) LsDir(dirPath string, readChildDir bool) (fileList []string, err error) {
+
+	rd, err := ioutil.ReadDir(dirPath)
+
+	if err != nil {
+		return fileList, err
+	}
+
+	for _, fi := range rd {
+		if fi.IsDir() {
+			// 递归读取子目录
+			if readChildDir {
+				childFileList, err := u.LsDir(strings.TrimRight(dirPath, "/")+"/"+fi.Name(), readChildDir)
+				if err != nil {
+					return fileList, err
+				}
+
+				for _, childFile := range childFileList {
+					fileList = append(fileList, fi.Name()+"/"+childFile)
+				}
+			}
+		} else {
+			fileList = append(fileList, fi.Name())
+		}
+	}
+
+	return fileList, nil
+}
+
+// CopyDir 递归拷贝目录
+// @param src 源目录
+// @param dst 目标目录
+// @return error
+func (u Dir) CopyDir(src string, dst string) error {
+	var err error
+	var fds []os.FileInfo
+	var srcInfo os.FileInfo
+
+	if srcInfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcInfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcFp := path.Join(src, fd.Name())
+		dstFp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = u.CopyDir(srcFp, dstFp); err != nil {
+				return err
+			}
+		} else {
+			// TODO: 需确认是否修改同包下的FileUtil
+			if err = util.FileUtil().CopyFile(srcFp, dstFp); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// CopyDirWithFix 递归拷贝目录
+// @param src 			源目录
+// @param dst 			目标目录
+// @param filePrefix 	文件前缀
+// @return error
+func (u Dir) CopyDirWithFix(src string, dst string, filePrefix string) error {
+	var err error
+	var fds []os.FileInfo
+	var srcInfo os.FileInfo
+
+	if srcInfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcInfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+
+	for _, fd := range fds {
+		srcFp := path.Join(src, fd.Name())
+		dstFp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = u.CopyDirWithFix(srcFp, dstFp, filePrefix); err != nil {
+				return err
+			}
+		} else {
+			// TODO: 需确认是否修改同包下的FileUtil
+			if err = util.FileUtil().CopyFileWithFix(srcFp, dstFp, filePrefix); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
